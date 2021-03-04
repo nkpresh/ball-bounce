829b1f8d5aca5eb3c327169d50a50979188ef35a
Nkwua Precious <nkwuap@gmail.com>
2021-03-04 11:11:47 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,180 @@
+let app;
+let ball;
+let player;
+var vx, vy;
+let firstPage;
+let winPage;
+let LosePage;
+let canvas = document.querySelector("#canvas");
+let button;
+var point = 0;
+
+app = new PIXI.Application({
+    width: 600,
+    height: 600,
+    backgroundColor: 0xAAAAAA,
+
+});
+app.view.x = window.width / 2;
+app.view.y = window.height / 2;
+
+//initialize pages
+firstPage = new PIXI.Container();
+LosePage = new PIXI.Container();
+winPage = new PIXI.Container();
+
+
+
+//create player
+player = new PIXI.Sprite.from("img/player.png");
+player.width = 150;
+player.height = 20;
+player.anchor.set(0.5);
+player.x = app.view.width / 2;
+player.y = app.view.height - 10;
+
+//create ball
+ball = new PIXI.Sprite.from("img/ball.png");
+ball.width = 30;
+ball.height = 30;
+ball.x = app.view.width / 2;
+ball.y = app.view.height / 2;
+ball.anchor.set(0.5);
+
+//button
+button = new PIXI.Sprite.from("img/playButton.png");
+button.height = 40;
+button.width = 140;
+button.x = app.view.width / 3;
+button.y = app.view.height / 2;
+button.interactive = true;
+
+//create first page
+let BlueRect = new PIXI.Graphics();
+BlueRect.beginFill(0x0000FF);
+
+BlueRect.drawRect(0, 0, app.view.width, app.view.height);
+firstPage.addChild(BlueRect);
+
+let text = new PIXI.Text("Welcome!!!");
+text.anchor.set(0.5);
+text.x = app.view.width / 2;
+text.y = app.view.height / 3;
+text.style = new PIXI.TextStyle({
+    fill: 0x111111,
+    fontSize: 80,
+    fontFamily: "Arcade"
+});
+
+//lose Page
+let NewRect = new PIXI.Graphics();
+NewRect.beginFill(0x00AAAA);
+
+NewRect.drawRect(0, 0, app.view.width, app.view.height);
+LosePage.addChild(NewRect);
+
+let text1 = new PIXI.Text(`YOU LOSE!!!  HIGH SCORE: ${point}`);
+text1.anchor.set(0.5);
+text1.x = app.view.width / 2;
+text1.y = app.view.height / 3;
+text1.width = 500;
+text1.style = new PIXI.TextStyle({
+    fill: 0x111111,
+    fontSize: 40,
+    fontFamily: "Arcade"
+});
+LosePage.addChild(text1);
+
+onload = function(e) {
+    e.preventDefault();
+    canvas.appendChild(app.view);
+    app.stage.addChild(player);
+    app.stage.addChild(ball);
+    app.stage.addChild(firstPage);
+    app.stage.addChild(LosePage);
+    app.stage.addChild(winPage);
+
+    LosePage.visible = false;
+    winPage.visible = false;
+
+    firstPage.addChild(text);
+    firstPage.addChild(button);
+
+}
+button.on("click", startGame);
+
+function startGame() {
+    firstPage.visible = false;
+    setInterval(moveBall, 1000 / 30);
+
+}
+app.stage.interactive = true;
+app.stage.on("pointermove", movePlayer);
+vx = Math.random() * 20 + 100 / 30;
+vy = Math.random() * 20 + 100 / 30;
+
+
+function movePlayer(e) {
+    let pos = e.data.global;
+    if (player.x - (pos.width / 2) < 0) {
+
+    } else {
+        player.x = pos.x;
+    }
+}
+
+function moveBall() {
+    ball.x += vx;
+    ball.y += vy;
+
+    if (hitPlayer(ball, player)) {
+        point += 10;
+        changeDirection();
+    }
+    if (ball.x + ball.width / 2 > app.view.width) {
+        vx = -vx;
+    }
+    if (ball.x - ball.width <= 0) {
+        vx = -vx;
+    }
+
+    if (ball.y - ball.height < 0) {
+        vy = -vy;
+    }
+
+    if (ball.y + ball.height / 2 >= app.view.height) {
+
+
+        app.stage.removeChild(ball);
+        app.stage.removeChild(player);
+        LosePage.visible = true;
+        point = null
+    }
+
+}
+
+function hitPlayer(bll, ply) {
+    return bll.x + bll.width * 3 > ply.x &&
+        ply.x + Math.floor(ply.width) >= ball.x &&
+        bll.y + bll.height > ply.y &&
+        ply.y + ply.height > ball.y;
+}
+
+function changeDirection() {
+
+    vx = -vx;
+    document.querySelector("#number").innerHTML = point;
+    vy = -vy;
+    // //right
+    // if (xv >= (player.width - player.width / 3) && xv <= 150) {
+    //     vx = +vx;
+    // }
+    // //center
+    // if (xv > 50 && xv < 100) {
+    //     vx = Math.random() * vx + -vy;
+    // }
+    // //left
+    // if (xv <= 50 && xv > 0) {
+    //     vx = -vx
+    // }
+}
